b7e4804299ee57f35fd5fe5b062320d3a1385f30
Pradeep-Prady <pradeepprady005@gmail.com>
2024-08-19 10:53:18 +0530

    host-check 2

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:7000",
   "http://127.0.0.1:7000",
+  "https://vjp-backend.onrender.com",
+  "http://vjp-backend.onrender.com",
 ];
 app.use(cookieParser());
 
